03fbba01e9940c9c951a3eb80013a66292905288
kmfdm9000 <ka4nevlxa@gmail.com>
2021-04-09 10:11:49 +0300

    add advantages

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,17 @@
 $(function(){
 
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+      });
     var mixer = mixitup('.products__inner-box');
-
-
+    
+      $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
+      });
 
 });
